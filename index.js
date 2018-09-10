600cfccad05d0ea53d26f33383165c500c671d9d
masterBaster <bartek.baster@gmail.com>
2018-09-10 15:51:31 +0200

    installed body-parser and sent some date to req.body using postman

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let express = require('express');
+let bodyParser = require('body-parser')
 let app = express();
 let routes = require('./routes/api');
 
+app.use(bodyParser.json());
 app.use('/api', routes);
 
 app.listen(process.env.port || 4000, function(){
--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,7 @@ router.get('/drivers', function(req, res){
 });
 //add a new driver to the db
 router.post('/drivers', function(req, res){
+    console.log(req.body)
     res.send({type: 'POST'});
 });
 //update a driver in the db
