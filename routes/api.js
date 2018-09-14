aa67b209641d0e8489d09b514a9c0e50116c299e
masterBaster <bartek.baster@gmail.com>
2018-09-14 18:26:21 +0200

    near function finally work properly

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,18 @@ let Driver = require('../models/drivers');
 
 //get the list of dirvers from the db
 router.get('/drivers', function(req, res, next){
-    Driver.find({}).then(function(drivers){
-        res.send(drivers);
-    })
+    /*Driver.find({}).then(function(drivers){
+        res.send(drivers);*/
+        Driver.aggregate().near({
+            near: [parseFloat(req.query.lng), parseFloat(req.query.lat)],
+            maxDistance: 100000,
+            spherical: true,
+            distanceField: "dist.calculated"
+           }).then(function(drivers){
+            res.send(drivers);
+        }).catch(next);
 });
+
 //add a new driver to the db
 router.post('/drivers', function(req, res, next){
     Driver.create(req.body).then(function(driver){
@@ -20,13 +28,13 @@ router.put('/drivers/:id', function(req, res, next){
         Driver.findOne({_id: req.params.id}).then(function(driver){
             res.send(driver);
         });
-    });
+    }).catch(next);
 });
 //delete driver from the db
 router.delete('/drivers/:id', function(req, res, next){
     Driver.findByIdAndRemove({_id: req.params.id}).then(function(dirver){
         res.send(dirver);
-    });
+    }).catch(next)
 });
 
 module.exports = router;
